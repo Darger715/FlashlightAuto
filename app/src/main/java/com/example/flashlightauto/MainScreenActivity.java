37e6b665cdbe11d771ad44375d972fbe0aac3511
Darger715 <kortkiy@bk.ru>
2020-08-05 22:21:39 +0300

    Flashlight turn off by activate screen - working.

--- app/src/main/java/com/example/flashlightauto/MainScreenActivity.java
+++ app/src/main/java/com/example/flashlightauto/MainScreenActivity.java
@@ -1,45 +1,59 @@
 package com.example.flashlightauto;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.os.Vibrator;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import java.io.IOException;
-import java.security.Policy;
 import java.util.List;
 
 public class MainScreenActivity extends AppCompatActivity {
 
+
     Button button_on_off;
     //true = on, false = off
-    boolean check_button;
-    Camera.Parameters parameters;
-    private Camera camera;
-    boolean check_service;
+    public static boolean check_button;
+    public static Camera.Parameters parameters;
+    public static Camera camera;
+    private BroadcastReceiver mReceiver = null;
+    boolean check_flashlight;
 
+
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
 
-
-        setRequestedOrientation (ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
         button_on_off = findViewById(R.id.mainScreenActivity_button_flashlight);
+
+        // initialize receiver
+        final IntentFilter filter = new IntentFilter(Intent.ACTION_SCREEN_ON);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        mReceiver = new ScreenReceiver();
+        registerReceiver(mReceiver, filter);
+
         button_on_off.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
 
                 //to off
                 if (check_button) {
@@ -57,45 +71,50 @@ public class MainScreenActivity extends AppCompatActivity {
             }
         });
 
-        //запуск Сервиса
-        (findViewById(R.id.mainScreenActivity_button_turnOnService)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean flag = check_service;
-                startService(new Intent(MainScreenActivity.this,CheckScreenService.class));
-            check_service = true;
-            if(flag != check_service)
-            {
-                Intent toCheckScreenService =  new Intent(MainScreenActivity.this,CheckScreenService.class);
-                toCheckScreenService.putExtra("1",check_service);
-                //startActivity(toCheckScreenService);
-
-            }
-            }
-        });
-
-        //выключение Сервиса
-        (findViewById(R.id.mainScreenActivity_button_turnOffService)).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                boolean flag = check_service;
-                stopService(new Intent(MainScreenActivity.this,CheckScreenService.class));
-                check_service = false;
-                if(flag != check_service)
-                {
-                    Intent toCheckScreenService =  new Intent(MainScreenActivity.this,CheckScreenService.class);
-                    toCheckScreenService.putExtra("1",check_service);
-                    //startActivity(toCheckScreenService);
-            }}
-        });
         boolean isCameraFlash = getApplicationContext().getPackageManager()
                 .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 
         camera = Camera.open();
     }
 
+    //receiver
+    @Override
+    protected void onPause() {
+        // when the screen is about to turn off
+        if (ScreenReceiver.wasScreenOn) {
+            // this is the case when onPause() is called by the system due to a screen state change
+            Log.e("MainScreenActivity", "SCREEN TURNED OFF");
+            //MainScreenActivity.setFlashLigthOn();
+            check_button = false;
 
-    private void setFlashLigthOn() {
+        }
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        // only when screen turns on
+        button_on_off.setBackgroundResource(R.drawable.flashlight_off);
+        if (!ScreenReceiver.wasScreenOn) {
+            // this is when onResume() is called due to a screen state change
+            Log.e("MainScreenActivity", "SCREEN TURNED ON");
+            MainScreenActivity.setFlashLightOff();
+            check_button = true;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        if (mReceiver != null) {
+            unregisterReceiver(mReceiver);
+            mReceiver = null;
+        }
+        super.onDestroy();
+    }
+
+
+    public static void setFlashLigthOn() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -128,7 +147,7 @@ public class MainScreenActivity extends AppCompatActivity {
     }
 
 
-    private void setFlashLightOff() {
+    public static void setFlashLightOff() {
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -150,9 +169,3 @@ public class MainScreenActivity extends AppCompatActivity {
     }
 }
 
-class BootBroadcast extends BroadcastReceiver{
-    @Override
-public void onReceive(Context context, Intent intent){
-        context.startService(new Intent(context, CheckScreenService.class));
-    }
-}
---
+++ app/src/main/java/com/example/flashlightauto/ScreenReceiver.java
@@ -0,0 +1,30 @@
+package com.example.flashlightauto;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class ScreenReceiver extends BroadcastReceiver{
+
+    public static boolean wasScreenOn = true;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        //экран выключен
+        if (intent.getAction().equals(Intent.ACTION_SCREEN_OFF)) {
+            //todo
+            //MainScreenActivity.setFlashLigthOn();
+            Log.e("ScreenReceiver", "Экран выключился");
+            wasScreenOn = false;
+        }
+        //экран включён
+        else if (intent.getAction().equals(Intent.ACTION_SCREEN_ON)) {
+            //todo
+           MainScreenActivity.setFlashLightOff();
+            Log.e("ScreenReceiver", "Экран включился");
+            wasScreenOn = true;
+        }
+    }
+}
+
